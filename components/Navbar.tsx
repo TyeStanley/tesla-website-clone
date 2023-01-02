7f0a5b136207dbf35d7770298bca695c8fbf8bc2
Tye Stanley <tyestanley090@gmail.com>
2023-01-02 13:21:28 -0800

    made navbar for tesla home page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,44 @@
 import Link from "next/link";
 import Image from "next/image";
 import teslaLogo from "../assets/teslaLogo.svg";
+import { models } from "../data/models.json";
 
 export default function Navbar() {
   return (
-    <nav className="bg-black fixed top-0 left-0 right-0 z-50 shadow-md">
-      <div className="container mx-auto flex items-center justify-between px-6">
-        <Link href="/">
-          <Image src={teslaLogo} width={130} height={20} alt="Tesla Name" />
+    <nav className="fixed top-0 left-0 right-0 z-50 p-3 shadow-md">
+      <div className="flex items-center justify-between">
+        <Link href="/" className="px-4">
+          <Image src={teslaLogo} width={120} alt="Tesla Logo" />
         </Link>
+        <ol className="hidden list-none lg:flex">
+          {models.map((model) => {
+            return (
+              <li
+                key={model.name}
+                className="rounded py-1 transition delay-75 ease-linear hover:bg-gray-200/[.70]"
+              >
+                <Link href={model.url} className="px-2">
+                  <span className="mx-2 text-[#171A20]">{model.name}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ol>
+        <ol className="flex list-none items-center">
+          <li className="hidden rounded py-1 transition delay-75 ease-linear hover:bg-gray-200/[.70] lg:block">
+            <Link href="/shop" className="px-2">
+              <span className="mx-2 text-[#171A20]">Shop</span>
+            </Link>
+          </li>
+          <li className="hidden rounded py-1 transition delay-75 ease-linear hover:bg-gray-200/[.70] lg:block">
+            <Link href="/teslaaccount" className="px-2">
+              <span className="mx-2 text-[#171A20]">Account</span>
+            </Link>
+          </li>
+          <li className="cursor-pointer rounded bg-gray-200/[.70] py-1 transition delay-75 ease-linear hover:bg-gray-300/[.70] lg:bg-transparent lg:hover:bg-gray-200/[.70]">
+            <button className="px-4 text-center text-[#171A20]">Menu</button>
+          </li>
+        </ol>
       </div>
     </nav>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,28 +6,4 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./components/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    screens: {},
-    colors: {
-      transparent: "transparent",
-      // black: '#000',
-      // white: '#fff',
-      // gray: {
-      //   100: '#f7fafc',
-      //   900: '#1a202c',
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-    },
-    extend: {
-      // spacing: {
-      //   "8xl": "96rem",
-      //   "9xl": "128rem",
-      // },
-      // borderRadius: {
-      //   "4xl": "2rem",
-      // },
-    },
-  },
-  plugins: [],
 };
