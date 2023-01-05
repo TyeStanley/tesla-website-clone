94c200124cd3fa0353807d8f4d617da564f9f024
Tye Stanley <tyestanley090@gmail.com>
2023-01-05 08:41:53 -0800

    created hero image, and added model y info along with 2 buttons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 import teslaLogo from "../assets/teslaLogo.svg";
-import { models } from "../data/models.json";
+import modelsInfo from "../data/models.json";
+
+const models = modelsInfo.models;
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 p-3 shadow-md">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-transparent p-3">
       <div className="flex items-center justify-between">
         <Link href="/" className="px-4">
           <Image src={teslaLogo} width={120} alt="Tesla Logo" />
@@ -15,28 +17,36 @@ export default function Navbar() {
             return (
               <li
                 key={model.name}
-                className="rounded py-1 transition delay-75 ease-linear hover:bg-gray-200/[.70]"
+                className="cursor-pointer rounded py-1 transition delay-75 ease-linear hover:bg-[#171A20]/[.09]"
               >
                 <Link href={model.url} className="px-2">
-                  <span className="mx-2 text-[#171A20]">{model.name}</span>
+                  <span className="mx-2 font-['Gotham'] text-[14px] text-[#171A20]">
+                    {model.name}
+                  </span>
                 </Link>
               </li>
             );
           })}
         </ol>
         <ol className="flex list-none items-center">
-          <li className="hidden rounded py-1 transition delay-75 ease-linear hover:bg-gray-200/[.70] lg:block">
+          <li className="hidden cursor-pointer rounded py-1 transition delay-75 ease-linear hover:bg-[#171A20]/[.09] lg:block">
             <Link href="/shop" className="px-2">
-              <span className="mx-2 text-[#171A20]">Shop</span>
+              <span className="mx-2 font-['Gotham'] text-[14px] text-[#171A20]">
+                Shop
+              </span>
             </Link>
           </li>
-          <li className="hidden rounded py-1 transition delay-75 ease-linear hover:bg-gray-200/[.70] lg:block">
+          <li className="hidden cursor-pointer rounded py-1 transition delay-75 ease-linear hover:bg-[#171A20]/[.09] lg:block">
             <Link href="/teslaaccount" className="px-2">
-              <span className="mx-2 text-[#171A20]">Account</span>
+              <span className="mx-2 font-['Gotham'] text-[14px] text-[#171A20]">
+                Account
+              </span>
             </Link>
           </li>
-          <li className="cursor-pointer rounded bg-gray-200/[.70] py-1 transition delay-75 ease-linear hover:bg-gray-300/[.70] lg:bg-transparent lg:hover:bg-gray-200/[.70]">
-            <button className="px-4 text-center text-[#171A20]">Menu</button>
+          <li className="cursor-pointer rounded bg-[#171A20]/[.07] py-1 transition delay-75 ease-linear hover:bg-[#171A20]/[.09] lg:bg-transparent">
+            <button className="px-4 text-center font-['Gotham'] text-[14px] text-[#171A20]">
+              Menu
+            </button>
           </li>
         </ol>
       </div>
